5fd190f2ac0da29402242ec3b2706214aa5f9f4e
Hakim Benoudjit <h.benoudjit@gmail.com>
2015-03-08 00:39:35 +0100

    - change menu color from gray to brown - alter layout

--- footer.php
+++ footer.php
@@ -8,16 +8,19 @@
  */
 ?>
 
-	</div><!-- #content -->
+        </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'freebsdbeginner' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'freebsdbeginner' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'freebsdbeginner' ), 'freebsdbeginner', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="site-info">
+                <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'freebsdbeginner' ) ); ?>">
+                    <?php printf( __( 'Proudly powered by %s', 'freebsdbeginner' ), 'WordPress' ); ?>
+                </a>
+                <span class="sep"> | </span>
+                <?php printf( __( 'Theme: %1$s by %2$s.', 'freebsdbeginner' ), 'freebsdbeginner', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+            </div><!-- .site-info -->
+        </footer><!-- #colophon -->
+    </div><!-- #page -->
+</div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -18,26 +18,24 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'freebsdbeginner' ); ?></a>
+<div id="wrapper">
+    <div id="page" class="hfeed site">
+        <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'freebsdbeginner' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+        <header id="masthead" class="site-header" role="banner">
+            <div class="site-branding">
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            </div><!-- .site-branding -->
 
-        <div class="menu-container">
             <nav id="site-navigation" class="main-navigation" role="navigation">
                 <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'freebsdbeginner' ); ?></button>
                 <?php
                     wp_nav_menu(array(
                         'theme_location' => 'primary',
-                        'after'          => '<li class="menu-item-separator">|</li>',
+                        //'after'          => '<li class="menu-item-separator"></li>',
                     ));
                 ?>
             </nav><!-- #site-navigation -->
-        </div><!-- .menu-container -->
-	</header><!-- #masthead -->
+        </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+        <div id="content" class="site-content">
