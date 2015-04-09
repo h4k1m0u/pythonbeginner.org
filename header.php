f8bcd3382f3bee3f866b062326a1fe7ce6f54895
Hakim Benoudjit <h.benoudjit@gmail.com>
2015-04-09 23:13:52 +0100

    - make the layout responsive

--- header.php
+++ header.php
@@ -27,7 +27,10 @@
             </div><!-- .site-branding -->
 
             <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'pythonbeginner' ); ?></button>
+                <button class="menu-toggle" aria-controls="menu" aria-expanded="false">
+                    <i class="fa fa-bars"></i>
+                </button>
+
                 <?php
                     wp_nav_menu(array(
                         'theme_location'    => 'primary',
